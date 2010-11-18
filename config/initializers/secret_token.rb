b40ef1f32a55703bcff32db1198dc2cdc1b2b2bf
Michael Economy <michael@michaeleconomy.com>
2010-11-17 21:59:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamerankr::Application.config.secret_token = '0510478596b887416345c3a34d3ed96d9238d72916bb90556bd764f0864aa6cd17c63bac3a479204464f8b60d3a1a8665c771654e859d03cb3dbf82aca260e47'
