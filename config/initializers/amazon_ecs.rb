99d293a549777244ea353760181e5437978285fe
Michael Economy <michael@michaeleconomy.com>
2011-04-01 21:49:07 -0700

    catch exceptions!!!

--- config/initializers/amazon_ecs.rb
+++ config/initializers/amazon_ecs.rb
@@ -2,6 +2,12 @@ require "amazon/ecs"
 
 
 Rails.application.config.after_initialize do
-  Amazon::Ecs.options = {:aWS_access_key_id => Secret[:aWS_access_key_id],
-    :aWS_secret_key => Secret[:aWS_secret_key]}
+  begin
+    key_id = Secret[:aWS_access_key_id]
+    secret = Secret[:aWS_secret_key]
+  rescue Exception => e
+    Rails.logger.info "error getting secrets: #{e}\n#{e.backtrace.join("\n")}"
+  end
+  Amazon::Ecs.options = {:aWS_access_key_id => key_id,
+    :aWS_secret_key => secret}
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   
   Rails.application.config.after_initialize do
-    provider :facebook, Secret[:facebook_app_id], Secret[:facebook_app_secret]
+    begin
+      app_id = Secret[:facebook_app_id]
+      secret = Secret[:facebook_app_secret]
+    rescue Exception => e
+      Rails.logger.info "error getting secrets: #{e}\n#{e.backtrace.join("\n")}"
+    end  
+    provider :facebook, app_id, secret
   end
 end
