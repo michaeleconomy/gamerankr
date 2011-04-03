b6e9ca96ee3141a5527d7db162647d0fc068bc2a
Michael Economy <michael@michaeleconomy.com>
2011-04-03 01:20:43 -0700

    less games bigger covers on the homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   
   def index
-    @hot_games = Port.limit(100).order('id desc').with_image
+    @hot_games = Port.limit(30).order('id desc').with_image
     @rankings = Ranking.limit(5).order('id desc').with_review
   end
   
