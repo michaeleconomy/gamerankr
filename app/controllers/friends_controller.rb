1d026476727cbc2e20bf66f926a530c7b516a907
Michael Economy <michael@michaeleconomy.com>
2011-04-12 23:05:35 -0700

    fixed facebook inviting

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   before_filter :require_sign_in
   def index
     @friends = User.find_all_by_id(friend_ids)
-    @facebook_friends = friends_not_on_gr_ids.paginate :page => params[:page]
+    @facebook_friends = friends_not_on_gr_ids
   end
   
 end
