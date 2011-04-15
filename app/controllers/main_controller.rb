c6128446e2beddc5e70f85c881cf27edc2cb5269
Michael Economy <michael@michaeleconomy.com>
2011-04-15 08:16:49 -0700

    show my reviews on the hp

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,8 @@ class MainController < ApplicationController
       limit(29).
       order('games.rankings_count desc').
       includes(:game).with_image
-    @rankings = Ranking.limit(5).order('id desc').with_review
+    @rankings = Ranking.limit(5).order('id desc').with_review  
+    get_rankings
     @genres = Genre.limit(20).order('game_genres_count desc')
   end
   
