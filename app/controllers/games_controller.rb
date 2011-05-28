96c4e27a3caf5a1180a234d99cd7deae0766b246
Michael Economy <michael@michaeleconomy.com>
2011-05-27 23:55:09 -0700

    more bugs with game show relating to friends....

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,8 +12,12 @@ class GamesController < ApplicationController
     get_rankings [@game]
     if signed_in?
       @friend_rankings = @game.rankings.find_all_by_user_id(friend_ids)
-      @all_rankings -= @friend_rankings
-      @all_rankings -= @user_rankings.values
+      @all_rankings.delete_if do |r|
+        @friend_rankings.include?(r)
+      end
+      @all_rankings.delete_if do |r|
+        r.user_id == current_user.id
+      end
     end
     @ranking = @user_rankings.values.first
   end
