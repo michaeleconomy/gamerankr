d4da02c8cb5bf6f9314f6b80d1a56df079e087b4
Michael Economy <michael@michaeleconomy.com>
2011-05-30 22:39:04 -0700

    homepage sorting bug...

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
       limit(30).
       order('games.rankings_count desc').
       includes(:game, :platform).with_image.shuffle
-    @rankings = Ranking.limit(5).includes(:shelves, :user, :port => :game).order('id desc').with_review  
+    @rankings = Ranking.limit(5).includes(:shelves, :user, :port => :game).order('rankings.id desc').with_review
     get_rankings
     @genres = Genre.limit(20).order('name')
   end
