d884d133cbe0bdb0e6e0bb96915cca0ed1c12117
Michael Economy <michael@michaeleconomy.com>
2011-06-11 16:19:46 -0700

    con't show the current users review twice

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -43,6 +43,7 @@ class RankingsController < ApplicationController
     @port = @ranking.port
     @user = @ranking.user
     @comment_moderator = @user.facebook_user.uid
+    get_rankings [@port]
   end
   
   def create
