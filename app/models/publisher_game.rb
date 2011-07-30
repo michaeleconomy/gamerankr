951a86965fd8b204947307a1b662440a0c80ac32
Michael Economy <michael@michaeleconomy.com>
2011-07-30 13:27:26 -0700

    bug with uniqueness constraint

--- app/models/publisher_game.rb
+++ app/models/publisher_game.rb
@@ -3,7 +3,7 @@ class PublisherGame < ActiveRecord::Base
   belongs_to :port
   belongs_to :publisher
   
-  validates_uniqueness_of :port_id, :scope => :publisher
+  validates_uniqueness_of :port_id, :scope => :publisher_id
   
   before_validation :set_game_id
   
