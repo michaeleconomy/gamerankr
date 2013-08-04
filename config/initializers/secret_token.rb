6b58625b0244421193dd25b42a275422e8f7297b
Michael Economy <meconomy@goodreads.com>
2013-08-03 19:21:50 -0700

    genrate random values for security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gamerankr::Application.config.secret_token = '0510478596b887416345c3a34d3ed96d9238d72916bb90556bd764f0864aa6cd17c63bac3a479204464f8b60d3a1a8665c771654e859d03cb3dbf82aca260e47'
+
+Rails.application.config.after_initialize do
+  Gamerankr::Application.config.secret_token = Secret['rails_secret_token'] ||= rand(2**512).to_s(16)
+  Gamerankr::Application.config.secret_key_base = Secret['rails_secret_key_base'] ||= rand(10**20).to_s(36)
+end
