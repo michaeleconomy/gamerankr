d85674f1766a3d52bf9938629746339156365f56
Michael Economy <meconomy@goodreads.com>
2013-08-13 21:22:42 -0700

    struggling to get heroku to compile my assets

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -3,6 +3,9 @@ class Secret < ActiveRecord::Base
   def self.[](key)
     secret = find_by_key(key)
     secret ? secret.value : nil
+  rescue Exception => e
+    logger.error "couldn't load key #{key}\n#{e}\n#{e.backtrace}"
+    nil
   end
   
   def self.[]=(key, value)
@@ -10,5 +13,8 @@ class Secret < ActiveRecord::Base
     secret.value = value
     secret.save
     value
+  rescue Exception => e
+    logger.error "couldn't set key #{key}\n#{e}\n#{e.backtrace}"
+    nil
   end
 end
