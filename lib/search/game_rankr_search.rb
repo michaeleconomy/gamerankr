cc4e6888fb2cbda7b12891467bb4f9425099b189
Michael Economy <meconomy@goodreads.com>
2017-11-07 16:09:09 -0800

    order search results by most popular

--- lib/search/game_rankr_search.rb
+++ lib/search/game_rankr_search.rb
@@ -5,6 +5,7 @@ class Search::GameRankrSearch
     games = Game.
     	includes(:ports).
     	where("lower(title) like ?", "%#{lower_case_query}%").
+    	order("rankings_count desc").
     	paginate(:page => page)
     
     WillPaginate::Collection.create(page, games.per_page, [games.total_entries, 100].min) do |pager|
