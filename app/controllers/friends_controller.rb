17dfe36b1f4fefe1d57cc2a88c261b5c51a29c47
Michael Economy <meconomy@goodreads.com>
2017-11-07 16:23:03 -0800

    fixes to friends page

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
   before_action :require_sign_in
   def index
-    @friends = User.find_all_by_id(friend_ids)
+    @friends = User.where(id: friend_ids)
     @facebook_friends = friends_not_on_gr_ids
   end
   
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( one_offs/hp.js )
+Rails.application.config.assets.precompile += %w( one_offs/hp.js one_offs/friends.js )
