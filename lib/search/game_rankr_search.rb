28f9096cef6a508db4f2391bb22a93c8109aef22
Michael Economy <meconomy@goodreads.com>
2017-11-24 15:35:19 -0800

    limit local search to exactly 10 pages

--- lib/search/game_rankr_search.rb
+++ lib/search/game_rankr_search.rb
@@ -8,7 +8,7 @@ class Search::GameRankrSearch
     	order("rankings_count desc, id desc").
     	paginate(:page => page)
     
-    WillPaginate::Collection.create(page, games.per_page, [games.total_entries, 100].min) do |pager|
+    WillPaginate::Collection.create(page, games.per_page, [games.total_entries, games.per_page * 10].min) do |pager|
       pager.replace(games)
     end
   end
