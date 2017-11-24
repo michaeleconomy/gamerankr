51c221d485233dcb964a99ee82337c101f34e487
Michael Economy <meconomy@goodreads.com>
2017-11-24 15:01:25 -0800

    remove genres from the homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,9 +5,12 @@ class MainController < ApplicationController
       limit(30).
       order('games.rankings_count desc').
       includes(:game, :platform).shuffle
-    @rankings = Ranking.limit(5).includes(:shelves, :user, :port => :game).order('rankings.id desc').with_review
+    @rankings = Ranking.
+      limit(5).
+      includes(:shelves, :user, :port => :game).
+      order('rankings.id desc').
+      with_review
     get_rankings
-    @genres = Genre.limit(20).order('name')
   end
   
   def fb_test
