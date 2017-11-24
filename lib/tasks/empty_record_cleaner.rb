9dde0ca32a7bb49663fbf289a27580a44823f4c1
Michael Economy <meconomy@goodreads.com>
2017-11-23 16:12:44 -0800

    method to remove amazon ports with no rankings

--- lib/tasks/empty_record_cleaner.rb
+++ lib/tasks/empty_record_cleaner.rb
@@ -8,5 +8,14 @@ class Tasks::EmptyRecordCleaner
 	def self.remove_ports_lacking_additional_data
 		ports = Port.where("additional_data_type = 'GiantBombPort' and additional_data_id not in (select id from giant_bomb_ports)").all
 		ports.collect(&:id)
+		#TODO - add other types
+	end
+
+	def self.remove_amazon_ports_without_rankings
+		Tasks::CountFixer.fix_port_rankings_counts
+		ports = Port.where("additional_data_type = 'AmazonPort' and rankings_count=0").all
+		ports.each(&:destroy)
+
+		ports.size
 	end
 end
