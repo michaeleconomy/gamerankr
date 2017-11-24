80777d31b68e06f77696f23ebfc0efb65827f9f5
Michael Economy <meconomy@goodreads.com>
2017-11-24 15:27:11 -0800

    steam cleaner

--- lib/tasks/empty_record_cleaner.rb
+++ lib/tasks/empty_record_cleaner.rb
@@ -18,4 +18,13 @@ class Tasks::EmptyRecordCleaner
 
 		ports.size
 	end
+
+
+	def self.remove_steam_ports_without_rankings
+		Tasks::CountFixer.fix_port_rankings_counts
+		ports = Port.where("additional_data_type = 'SteamPort' and rankings_count=0").all
+		ports.each(&:destroy)
+
+		ports.size
+	end
 end
