2b0a25692a14de57bf0608ebb26800c0a4b77b66
Michael Economy <meconomy@goodreads.com>
2017-11-26 09:40:00 -0800

    tasks to remove android ports also

--- lib/tasks/empty_record_cleaner.rb
+++ lib/tasks/empty_record_cleaner.rb
@@ -27,4 +27,13 @@ class Tasks::EmptyRecordCleaner
 
 		ports.size
 	end
+
+
+	def self.remove_android_ports_without_rankings
+		Tasks::CountFixer.fix_port_rankings_counts
+		ports = Port.where("additional_data_type = 'AndroidMarketplacePort' and rankings_count=0").all
+		ports.each(&:destroy)
+
+		ports.size
+	end
 end
