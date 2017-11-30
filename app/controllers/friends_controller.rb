751ee733099769b2b9ee55000beefd99860e14a8
Michael Economy <meconomy@goodreads.com>
2017-11-30 11:21:35 -0800

    eager load the friends page adequately

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,14 @@
 class FriendsController < ApplicationController
   before_action :require_sign_in
   def index
-    @friends = User.where(id: friend_ids)
+    @friends = User.where(id: friend_ids).includes(:facebook_user)
     @facebook_friends = friends_not_on_gr_ids
     @updates = Ranking.where(user_id: friend_ids).
+      includes(:game, :shelves, {:user => :facebook_user, :port => [:platform, :additional_data]}).
       order("id desc").
       paginate(:page => params[:page])
+
+    get_rankings(@updates)
   end
   
 end
