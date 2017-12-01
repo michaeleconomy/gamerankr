9bf6ee006676523bd32b5ab48520d35c90d594bc
Michael Economy <meconomy@goodreads.com>
2017-12-01 08:29:17 -0800

    eager loading on the home page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,10 +2,10 @@ class MainController < ApplicationController
   
   def index
     port_ids = Ranking.where("created_at > ?", 3.months.ago).group(:game_id).order("count(1) desc").limit(30).pluck("min(port_id)")
-    @hot_games = Port.where(id: port_ids).includes(:game, :platform)
+    @hot_games = Port.where(id: port_ids).includes(:additional_data, :game, :platform)
     @rankings = Ranking.
       limit(10).
-      includes(:shelves, :user, :port => :game).
+      includes(:shelves, :user, :port => [:additional_data, :game, :platform]).
       order('rankings.id desc').
       with_review
     get_rankings
