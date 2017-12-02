d895fde832b3bcf1ec7b10a08e59580077366551
Michael Economy <meconomy@goodreads.com>
2017-12-02 15:31:09 -0800

    https itunes images

--- app/models/itunes_port.rb
+++ app/models/itunes_port.rb
@@ -7,16 +7,22 @@ class ItunesPort < ActiveRecord::Base
   
   def resized_image_url(size)
     size_px = size.gsub(/[^\d]/, "").to_i
-    if size_px <= 57
-      small_image_url
-    elsif size_px <= 100
-      medium_image_url
-    else
-      large_image_url
-    end
+    httpsify_url(
+      if size_px <= 57
+        small_image_url
+      elsif size_px <= 100
+        medium_image_url
+      else
+        large_image_url
+      end
+    )
   end
   
   def affiliate_url
     url
+  end
+
+  def httpsify_url(url)
+    url.gsub(/http(.*?)\./, "https\\1-ssl.")
   end
 end
