f927c06c8126a05768c8e324b6d2e0656a1f7e69
Michael Economy <meconomy@goodreads.com>
2017-12-30 08:45:48 -0800

    friend rankings

--- app/graphql/types/game_type.rb
+++ app/graphql/types/game_type.rb
@@ -7,9 +7,19 @@ Types::GameType = GraphQL::ObjectType.define do
       obj.rankings.order("id desc")
     end
   end
+  field :friend_rankings, !types[!Types::RankingType] do
+    resolve -> (obj, args, ctx) do
+      if ctx[:current_user] == FakeCurrentUser
+        []
+      else
+        obj.rankings.where(user_id: ctx[:current_user].friend_user_ids).order("id desc")
+      end
+    end
+  end
   field :ports, !types[!Types::PortType] do
     resolve -> (obj, args, ctx) do
       HasManyRecordLoader.for(Port, :game_id).load(obj.id)
     end
   end
+
 end
