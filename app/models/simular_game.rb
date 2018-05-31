5014bbe18227026d45277064870e3497f07e4302
Michael Economy <meconomy@goodreads.com>
2018-05-31 12:37:18 -0700

    recs v2

--- app/models/simular_game.rb
+++ app/models/simular_game.rb
@@ -1,21 +1,24 @@
-#note abotu version numbers - two algorithms cannot share a version number!
 class SimularGame < ApplicationRecord
   belongs_to :game
   belongs_to :simular_game, :foreign_key => "simular_game_id", :class_name => "Game"
 
   def self.get_for_game(game)
-    game.simular_games.where(version: current_version).order("score desc")
+    game.simular_games.where(version: current_version(SimularGames::UsersAlsoRatedHighly::ALGORITHM_ID)).order("score desc")
   end
 
   def self.generate
     SimularGames::UsersAlsoRatedHighly.generate
   end
 
-  def self.current_version
-    maximum(:version)
+  def self.current_version(algorithm_id)
+    where(algorithm: algorithm_id).maximum(:version)
   end
 
-  def self.get_next_version
-    (current_version || 0) + 1
+  def self.get_next_version(algorithm_id)
+    (current_version(algorithm_id) || 0) + 1
+  end
+
+  def self.delete_old_versions(algorithm_id)
+    where(algorithm: algorithm_id).where("version < ?", current_version(algorithm_id)).delete_all
   end
 end
--- lib/simular_games/users_also_rated_highly.rb
+++ lib/simular_games/users_also_rated_highly.rb
@@ -2,23 +2,28 @@ class SimularGames::UsersAlsoRatedHighly
 
   ALGORITHM_ID = 1
 
-
   def self.generate
+    new.go
+  end
+
+  def go
     raw_rankings = get_raw_rankings
     simularity_counts = get_simularity_counts(raw_rankings)
     normalize_by_ranking_count(simularity_counts)
 
     related_games = limit_to_best(simularity_counts)
     write_to_db(related_games)
+    SimularGame.delete_old_versions(ALGORITHM_ID)
     nil
   end
 
-  def self.get_raw_rankings
+
+  def get_raw_rankings
     Rails.logger.info("grabbing raw rankings from db")
     Ranking.where("ranking is null or ranking in (4, 5)").pluck(:user_id, :game_id, :ranking)
   end
 
-  def self.get_simularity_counts(rankings)
+  def get_simularity_counts(rankings)
     Rails.logger.info "grouping by user"
     rankings_grouped_by_user = Hash.new{|hash, key| hash[key] = []}
     rankings.each do |user_id, game_id, rating|
@@ -44,41 +49,65 @@ class SimularGames::UsersAlsoRatedHighly
     simularity_counts
   end
 
-  def self.limit_to_best(simularity_counts)
+  def limit_to_best(simularity_counts)
     Rails.logger.info "limiting to best"
     simularity_counts.collect do |game_id, simularities|
       simluarities_limited =
-        simularities.to_a.shuffle.sort_by{|id, score| -score}.first(10)
+        simularities.to_a.shuffle.sort_by{|id, score| [-score, -rankings_count(id), -average_rating(id)]}.first(10)
       [game_id, simluarities_limited]
     end
   end
 
-  
-  def self.normalize_by_ranking_count(simularity_counts)
-    rankings_counts = get_rankings_counts
-
+  def normalize_by_ranking_count(simularity_counts)
     Rails.logger.info "normalizing by rankings count"
     simularity_counts.each do |game_id, simularities|
       simularities.each do |other_game_id, score|
-        simularities[other_game_id] = score / rankings_counts[other_game_id]
+        simularities[other_game_id] = score / rankings_count(other_game_id)
       end
     end
   end
 
-  def self.get_rankings_counts
+  def rankings_count(game_id)
+    if !@rankings_counts
+      fetch_rankings_counts
+    end
+    @rankings_counts[game_id]
+  end
+
+  def average_rating(game_id)
+    unless @average_ratings
+      load_average_ratings
+    end
+    @average_ratings[game_id] || 0
+  end
+
+  def load_average_ratings
+    Rails.logger.info "grabbing aberage ratings from db"
+    @average_ratings = {}
+    raw_ratings =
+      Ranking.where("ranking is not null").
+      group(:game_id).
+      pluck(:game_id, Arel.sql("avg(ranking)"))
+    Rails.logger.info "hashing average ratings"
+    raw_ratings.each do |id, avg|
+      @average_ratings[id] = avg
+    end
+  end
+
+  def fetch_rankings_counts
     Rails.logger.info "grabbing rankings_counts from db"
     rankings_counts_raw = Game.pluck(:id, :rankings_count)
     Rails.logger.info "hashing rankings count"
-    rankings_counts = {}
+    @rankings_counts = {}
     rankings_counts_raw.each do |id, count|
-      rankings_counts[id] = count
+      @rankings_counts[id] = count
     end
-    rankings_counts
+    @rankings_counts
   end
 
-  def self.write_to_db(related_games)
+  def write_to_db(related_games)
     Rails.logger.info "writing simular games to database"
-    version = SimularGame.get_next_version
+    version = SimularGame.get_next_version(ALGORITHM_ID)
     related_games.each do |game_id, simularities|
       simularities.each do |simular_game_id, score|
         sg = SimularGame.new
