ebf205866fa9dd28b8a88aecc5648e354d551815
Michael Economy <michaeleconomy@users.noreply.github.com>
2020-03-16 09:08:02 -0700

    fix giant bomb images

--- app/models/giant_bomb_images.rb
+++ app/models/giant_bomb_images.rb
@@ -1,7 +1,11 @@
 module GiantBombImages
 
-  IMAGE_PREFIX = "https://www.giantbomb.com/api/image/"
-  
+  IMAGE_PREFIX_OLD = "https://www.giantbomb.com/api/image/"
+  IMAGE_PREFIX = "https://giantbomb1.cbsistatic.com/uploads/"
+
+  # https://giantbomb1.cbsistatic.com/uploads/scale_small/0/3699/2749429-assassin%27s%20creed%20-%20syndicate.jpg
+  # https://www.giantbomb.com/api/image/scale_small/2732892-rsz_1image.jpg
+  # https://giantbomb1.cbsistatic.com/uploads/scale_small/18/187968/2732892-rsz_1image.jpg
 
   def resized_image_url(size)
     size_px = size.gsub(/[^\d]/, "").to_i
@@ -13,15 +17,15 @@ module GiantBombImages
   end
   
   def icon_image_url
-    "#{IMAGE_PREFIX}square_avatar/#{image_id}"
+    "#{image_prefix}square_avatar/#{image_id}"
   end
 
   def medium_image_url
-    "#{IMAGE_PREFIX}scale_small/#{image_id}"
+    "#{image_prefix}scale_small/#{image_id}"
   end
 
   def original_image_url
-    "#{IMAGE_PREFIX}original/#{image_id}"
+    "#{image_prefix}original/#{image_id}"
   end
 
   def large_image_url
@@ -31,4 +35,14 @@ module GiantBombImages
   def small_image_url
     icon_image_url
   end
+
+  private
+
+  def image_prefix
+    if image_id.include?("/")
+      return IMAGE_PREFIX
+    end
+    IMAGE_PREFIX_OLD
+  end
+
 end
