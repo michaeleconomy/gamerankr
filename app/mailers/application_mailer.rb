0980ba09b9ff01864e58f8c0ecea4f7e3386574f
Michael Economy <michaeleconomy@users.noreply.github.com>
2022-06-05 17:19:35 -0700

    more upgrade issues :

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mail_layout'
-  add_template_helper(ApplicationHelper)
+  helper ApplicationHelper
   default :from => "GameRankr <no-reply@gamerankr.com>"
 end
