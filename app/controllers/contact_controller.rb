72def4d776cffa27dc80fd71ab3db0320a2e0c24
Michael Economy <michaeleconomy@users.noreply.github.com>
2022-06-24 15:29:03 -0700

    captcha on contact form signed out

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,11 +2,41 @@ class ContactController < ApplicationController
 
   before_action :require_sign_in, :except => [:index, :submit]
   
-  def index
 
+  @@captchas = {
+    "What color clothing does Mario wear?": ["red"],
+    "What is Mario's profession?": ["plumber"],
+    "Sonic is what kind of animal?": ["hedgehog", "hedge hog"],
+    "What color is Sonic?": ["blue"],
+    "What is Sonic's partner's name?": ["tails"],
+    "Who is Mario's brother?": ["luigi"],
+    "Name an extremely popular electric rat pokemon": ["pikachu"],
+    "This popular nintendo franchise is a portmanteau of the words 'pocket monsters'": ["pokemon", "pokémon"],
+    "This plumber is possibly nintendo's most popular character": ["mario"],
+    "The name of the protagonist of the Zelda games": ["link"],
+  }
+  def index
+    if signed_out?
+      @captcha = session[:captcha] = @@captchas.keys.sample
+    end
   end
 
   def submit
+    if signed_out?
+      answers = @@captchas[session[:captcha]]
+      cleaned_answer = (params[:answer] || "").strip.downcase
+      if !answers.include?(cleaned_answer)
+        flash[:error] = "incorrect answer to the captcha question"
+        redirect_to contact_path
+        return
+      end
+    end
+    email = params[:email]
+    if !Email.regex.match?(email)
+      flash[:error] = "Invalid email address provided"
+      redirect_to contact_path
+      return
+    end
     data = {
       email: params[:email],
       subject: params[:subject],
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,10 +1,16 @@
 class Email < ActiveRecord::Base
+
+  def self.regex
+    /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  end
+
   belongs_to :user
-  validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates_format_of :email, :with => regex
   validates_uniqueness_of :email
   after_save :if => lambda {saved_change_to_primary && primary} do
     user.emails.where("id != ?", id).update_all(primary: false)
   end
+
     
   before_save do
     if email_changed?
