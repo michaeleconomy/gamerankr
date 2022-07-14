e99e64bdafc558df693c49d28df41f352bf5d80b
Michael Economy <michaeleconomy@users.noreply.github.com>
2022-07-14 16:04:37 -0700

    api bug

--- test/api/api_test.rb
+++ test/api/api_test.rb
@@ -156,17 +156,123 @@ class ApiTest < ActionDispatch::IntegrationTest
 		assert result['data']['user']['real_name'] == user.real_name
 	end
 
+	test "signed_in game view" do
+		user = sign_in_api
+
+		r = create_ranking user: user
+
+	  query = <<-GRAPHQL
+			query Game($id: ID!) {
+			  game(id: $id) {
+			    __typename
+			    ...GameBasic
+			    ports {
+			      __typename
+			      id
+			      medium_image_url
+			      description
+			    }
+
+			    rankings(first: 30) {
+			      __typename
+			      edges {
+			        __typename
+			        node {
+			          __typename
+			          ...RankingWithUser
+			        }
+			      }
+			      pageInfo {
+			        __typename
+			        endCursor
+			        hasNextPage
+			      }
+			    }
+			    friend_rankings {
+			      __typename
+			      ...RankingWithUser
+			    }
+			  }
+		  }
+			fragment GameBasic on Game {
+			  __typename
+			  id
+			  title
+			  url
+			  ports {
+			    __typename
+			    id
+			    platform {
+			      __typename
+			      id
+			      name
+			    }
+			    small_image_url
+			  }
+		  }
+			fragment RankingWithUser on Ranking {
+			  __typename
+			  ...RankingBasic
+			  user {
+			    __typename
+			    ...UserBasic
+			  }
+		  }
+			fragment RankingBasic on Ranking {
+			   __typename
+			   id
+			   ranking
+			   review
+			   verb
+			   comments_count
+			   url
+			   shelves {
+			     __typename
+			     ...ShelfBasic
+			   }
+			   port {
+			    __typename
+			    id
+			    platform {
+			      __typename
+			      id
+			      name
+			    }
+			    small_image_url
+			  }
+		  }
+			fragment ShelfBasic on Shelf {
+			  __typename
+			  id
+			  name
+			}
+			fragment UserBasic on User {
+			  __typename
+			  id
+			  real_name
+			  photo_url
+		  }
+	  GRAPHQL
+		result = execute_query(query, variables: { id: r.game_id })
+		# puts result.inspect
+
+		assert result['data']['game']['title'] = r.game.title
+		assert result['data']['game']['friend_rankings'].is_a?(Array)
+		assert result['data']['game']['rankings']['edges'].is_a?(Array)
+		assert result['data']['game']['rankings']['edges'].size == 1
+	end
+
 
 
 	private
 
-	def execute_query(query)
+	def execute_query(query, variables: nil)
 		headers = {}
 		if @api_token
 			headers["api-token"] = @api_token
 		end
 		post graphql_path,
-			params: {query: query},
+			params: {query: query, variables: variables},
 			headers: headers
 		assert_response 200
 		JSON.parse(@response.body)
