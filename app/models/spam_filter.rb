478f7efe9d54b75ee11069dedf47f14307953c70
Michael Economy <michaeleconomy@users.noreply.github.com>
2022-08-03 10:49:15 -0700

    spam filter case insensative

--- app/models/spam_filter.rb
+++ app/models/spam_filter.rb
@@ -1,5 +1,6 @@
 class SpamFilter < ApplicationRecord
 	def self.filter(s)
-		all.collect(&:keyword).any?{|keyword| s.include?(keyword)}
+		s = s.downcase
+		all.collect{|f| f.keyword.downcase }.any?{|keyword| s.include?(keyword)}
 	end
 end
