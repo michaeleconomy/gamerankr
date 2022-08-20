9cfee190fc3dbfb0560df684c634887a69007fd5
Michael Economy <michaeleconomy@users.noreply.github.com>
2022-08-19 19:12:11 -0700

    expose platform shortNames to api

--- app/graphql/types/platform_type.rb
+++ app/graphql/types/platform_type.rb
@@ -2,5 +2,9 @@ class Types::PlatformType < Types::BaseObject
   graphql_name "Platform"
   field :id, ID, null: false
   field :name, String, null: false
+  field :short_name, String, null: false
+  def short_name
+    object.short
+  end
   # field :ports, [Types::PortType, null: false], null: false
 end
