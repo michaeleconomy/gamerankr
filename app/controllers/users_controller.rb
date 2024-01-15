d53a095bdcb1c1b252e577929b0cb96bd047ade0
Michael Economy <michaeleconomy@users.noreply.github.com>
2024-01-14 16:49:49 -0800

    fix bug with user search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
 
     if User.email_regex.match?(params[:query])
-      @users = [User.where("lower(email) = lower(?)", params[:query])].compact
+      @users = [User.where("lower(email) = lower(?)", params[:query]).first].compact
     else
       @users = User.where("unaccent(lower(real_name)) like unaccent(lower(?))", "%#{params[:query]}%").
         paginate(page: params[:page])
