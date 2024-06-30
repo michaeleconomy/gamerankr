f159e5506ceed6236c8a26247f6659ff6b990094
Michael Economy <michaeleconomy@users.noreply.github.com>
2024-06-30 13:37:53 -0700

    ignore Net::SMTPSyntaxError errors from the mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,8 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mail_layout'
   helper ApplicationHelper
   default from: "GameRankr <no-reply@gamerankr.com>"
+
+  rescue_from "Net::SMTPSyntaxError" do |err|
+    logger.info "Net::SMTPSyntaxError: #{err}"
+  end
 end
